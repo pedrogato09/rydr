4722bbec12337e3c36a15c5b4ff7184f433b5023
pedrogato09 <9028928@student.zadkine.nl>
2026-03-27 10:58:41 +0100

    Merge remote-tracking branch 'origin/main' allen positie veranderen
    filter locatie tijd en datum

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,53 @@
+<div class="filter-container">
+    <div class="filter-card">
+        <div class="radio-group">
+            <input type="radio" checked> <label>Pick – Up</label>
+        </div>
+        <div class="inputs-row">
+            <div class="input-group">
+                <label>Locations</label>
+                <select><option>Select your city</option></select>
+            </div>
+            <div class="input-group">
+                <label>Date</label>
+                <input type="date" placeholder="Select your date">
+            </div>
+            <div class="input-group">
+                <label>Time</label>
+                <input type="time" placeholder="Select your time">
+            </div>
+        </div>
+    </div>
+
+    <div class="swap-button">
+        <i class="fas fa-arrows-alt-v"></i> </div>
+
+    <div class="filter-card">
+        <div class="radio-group">
+            <input type="radio" checked> <label>Drop – Off</label>
+        </div>
+        <div class="inputs-row">
+            <div class="input-group">
+                <label>Locations</label>
+                <select><option>Select your city</option></select>
+            </div>
+            <div class="input-group">
+                <label>Date</label>
+                <input type="date" placeholder="Select your date">
+            </div>
+            <div class="input-group">
+                <label>Time</label>
+                <input type="time" placeholder="Select your time">
+            </div>
+        </div>
+    </div>
+    
+    <button class="search-btn">🔍</button>
+</div>
+
+
+
+
 <?php require "includes/header.php"; ?>
 <?php require "database/connection.php"; ?>
 
